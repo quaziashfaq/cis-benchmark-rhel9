ee5590523f190ab2cd74b9a0de365d3dfc102329
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2023-08-04 11:52:14 +0800

    squashfs and udf filesystems are disabled

---
+++ 1-initial-setup/1.1-filesystem-configuration/1.1-audit-filesystem.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# 1.1 Filesystem Configuration
+
+# 1.1.1 Disable unused filesystems
+./1.1.1.1-audit-ensure-mounting-of-squashfs-filesystems-is-disabled.sh
+./1.1.1.2-audit-ensure-mounting-of-udf-filesystems-is-disabled.sh
+
+# 1.1.2 Configure /tmp
+# 1.1.3 Configure /var
+# 1.1.4 Configure /var/tmp
+# 1.1.5 Configure /var/log
+# 1.1.6 Configure /var/log/audit
+# 1.1.7 Configure /home
+# 1.1.8 Configure /dev/shm
+
+
