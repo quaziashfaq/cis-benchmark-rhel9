e2edfb1bdff80dad00bb99ef1502c45fef0b1af1
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2023-08-08 19:05:29 +0800

    correcting nodev option settin on /var/log

--- 1-initial-setup/1.1-filesystem-configuration/1.1.5.2-audit-ensure-nodev-option-set-on-var-log-partition.sh
+++ 1-initial-setup/1.1-filesystem-configuration/1.1.5.2-audit-ensure-nodev-option-set-on-var-log-partition.sh
@@ -1,16 +1,21 @@
-1.1.5.2 Ensure nodev option set on /var/log partition
-Audit:
-Verify that the nodev option is set for the /var/log mount.
-Run the following command to verify that the nodev mount option is set.
-Example:
-# findmnt --kernel /var/log | grep nodev
-/var/log /dev/sdb ext4 rw,nosuid,nodev,noexec,relatime,seclabel
-Remediation:
-Edit the /etc/fstab file and add nodev to the fourth field (mounting options) for the 
-/var/log partition.
-Example:
-<device> /var/log <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 
-0
-Run the following command to remount /var/log with the configured options:
-# mount -o remount /var/log
-References:
+#!/usr/bin/env bash
+
+
+# 1.1.5.2 Ensure nodev option set on /var/log partition
+# /var/log /dev/sdb ext4 rw,nosuid,nodev,noexec,relatime,seclabel
+
+source ../../utilities.sh
+
+audit_ensure_nodev_option_set_on_var_log_partition() {
+
+    echo -n "1.1.5.2 Ensure nodev option set on /var/log partition: "
+
+    if [[ "$(findmnt --kernel /var/log | grep nodev | wc -l)" -eq 1 ]]; then
+        test_passed "Pass"
+    else
+        test_failed "Fail"
+    fi
+}
+
+audit_ensure_nodev_option_set_on_var_log_partition
+
