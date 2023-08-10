6335dde0df732c312b8f00f4ac02ea2701c8b1b8
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2023-08-10 12:04:15 +0800

    nosuid option set on /var/log/audit

---
+++ 1-initial-setup/1.1-filesystem-configuration/1.1.6.4-audit-ensure-nosuid-option-set-on-var-log-audit-partition.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# 1.1.6.4 Ensure nosuid option set on /var/log/audit partition
+# /var/log/audit /dev/sdb ext4 rw,nosuid,nodev,noexec,relatime,seclabel
+
+source ../../utilities.sh
+
+audit_ensure_nosuid_option_set_on_var_log_audit_partition() {
+
+    echo -n "1.1.6.4 Ensure nosuid option set on /var/log/audit partition: "
+
+    if [[ "$(findmnt --kernel /var/log/audit | grep nosuid | wc -l)" -eq 1 ]]; then
+        test_passed "Pass"
+    else
+        test_failed "Fail"
+    fi
+}
+
+
+audit_ensure_nosuid_option_set_on_var_log_audit_partition
+
