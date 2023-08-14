4e2322f44715b2c76d229d7c29edde21a89a46c7
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2023-08-14 18:36:42 +0800

    remediate ensuring gpg key are configured

--- 1-initial-setup/1.2-configure-software-updates/1.2.1-remediate-ensure-gpg-keys-are-configured-manual.sh
+++ 1-initial-setup/1.2-configure-software-updates/1.2.1-remediate-ensure-gpg-keys-are-configured-manual.sh
@@ -4,3 +4,17 @@ echo ""
 echo "-------------------------------------------------------------------------------"
 echo "Update your package manager GPG keys in accordance with site policy."
 echo "-------------------------------------------------------------------------------"
+
+
+#sed -i 's/^gpgcheck\s*=\s*.*/gpgcheck=1/' /etc/dnf/dnf.conf
+
+
+
+#find /etc/yum.repos.d/ -name "*.repo" -exec echo "Checking:" {} \; -exec sed -i 's/^gpgcheck\s*=\s*.*/gpgcheck=1/' {} \;
+
+
+find /etc/yum.repos.d/ -name "*.repo" \
+        -exec echo -n "Backing up: " \; \
+        -exec cp -pv {} "{}.bkp.$(date '+%Y%m%dT%H%M')" \; \
+        -exec echo "Checking:" {} \; \
+        -exec sed -i 's/^gpgcheck\s*=\s*.*/gpgcheck=1/' {} \;
